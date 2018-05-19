067ebb3cbdc4b6f0f4e2bf61127775afdc68ed39
vinodmuli <mulivinod1994@gmail.com>
2018-05-19 15:44:23 +0530

    Changed configurations

--- src/main/java/online/music/store/record/config/SecurityWebConfiguration.java
+++ src/main/java/online/music/store/record/config/SecurityWebConfiguration.java
@@ -3,30 +3,10 @@
  */
 package online.music.store.record.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 /**
  * @author vinod
  *
  */
-@Configuration
-@EnableWebSecurity
-public class SecurityWebConfiguration extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private DataSource dataSource;
-
-	@Autowired
-	public void configAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-		authenticationManagerBuilder.jdbcAuthentication().dataSource(dataSource)
-				.usersByUsernameQuery("select username,password, enabled from users where username=?")
-				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
-	}
+public class SecurityWebConfiguration {
 
 }
